2f559402d76ad03661b099d285ce82ad039fe000
Sulaiman631437005 <sulaiman.jaraae@gmail.com>
2022-08-15 08:15:16 +0700

    /

--- Web Programming/js/script.js
+++ Web Programming/js/script.js
@@ -4,14 +4,14 @@ menuToggle.onclick = function() {
   Navbar.classList.toggle('active');
 }
 
-let list = document.querySelectorAll('.nav-link');
-for (let i=0; i<list.length; i++){
-  list[i].onclick = function() {
-    let j = 0;
-    while(j < list.length){
-      list[j++].className = 'nav-link';
-    }
-    list[i].className = 'nav-link active';
-  }
-}
+// let list = document.querySelectorAll('.nav-link');
+// for (let i=0; i<list.length; i++){
+//   list[i].onclick = function() {
+//     let j = 0;
+//     while(j < list.length){
+//       list[j++].className = 'nav-link';
+//     }
+//     list[i].className = 'nav-link active';
+//   }
+// }
 
