3615e575bdff2ea6d4305eaea1f58e3f66fc7bad
Sulaiman631437005 <sulaiman.jaraae@gmail.com>
2022-09-21 10:17:48 +0700

    /

--- Myweb/Personal/admin.py
+++ Myweb/Personal/admin.py
@@ -2,7 +2,4 @@ from django.contrib import admin
 from .models import MemberImages
 # Register your models here.
 
-class  MemberAdmin(admin.ModelAdmin):
-    readonly_fields = ('id',)
-
-admin.site.register(MemberImages, MemberAdmin)
+admin.site.register(MemberImages)
